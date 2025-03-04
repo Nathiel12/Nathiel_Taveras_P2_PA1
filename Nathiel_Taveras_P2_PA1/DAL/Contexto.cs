5e87e46b555d4359837e55ee4e8d5b4ddad61401
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-04 21:55:41 +0100

    Crear contexto

---
+++ Nathiel_Taveras_P2_PA1/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nathiel_Taveras_P2_PA1.Models;
+
+namespace Nathiel_Taveras_P2_PA1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Modelo> Modelo { get; set; }
+    }
+}
