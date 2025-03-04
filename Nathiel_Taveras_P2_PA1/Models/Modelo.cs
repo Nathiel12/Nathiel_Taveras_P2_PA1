996d7f04f70586d4d029e50fe60bae08caadc317
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-04 21:55:52 +0100

    Crear modelo

---
+++ Nathiel_Taveras_P2_PA1/Models/Modelo.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nathiel_Taveras_P2_PA1.Models
+{
+    public class Modelo
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
