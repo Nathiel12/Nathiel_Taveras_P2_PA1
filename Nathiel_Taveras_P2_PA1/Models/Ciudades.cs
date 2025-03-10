d96f87760357bd0ebf4e2db89061b8fa5404c215
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-10 23:23:32 +0100

    Crear modelo ciudades

---
+++ Nathiel_Taveras_P2_PA1/Models/Ciudades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nathiel_Taveras_P2_PA1.Models
+{
+    public class Ciudades
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public double Monto { get; set; } = 0;
+    }
+}
